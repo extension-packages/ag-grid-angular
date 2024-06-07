088026435e1a389b5749b1094ce9d3ae65553607
Benjamin Reiersølmoen <bdev@tuta.com>
2024-06-07 22:21:04 +0200

    Updates

--- projects/extension-packages/ag-grid-angular/.storybook/theme.js
+++
@@ -1,9 +0,0 @@
-import { create } from "@storybook/theming/create";
-
-export default create({
-  base: "light",
-  brandTitle: "@expansion-packages/ag-grid-angular",
-  brandUrl: "https://github.com/expansion-packages/ag-grid-angular",
-  brandImage: undefined,
-  brandTarget: "_self",
-});
--- projects/extension-packages/ag-grid-angular/src/lib/ag-grid-extension/interfaces/ag-grid-context.ts
+++
@@ -1,6 +0,0 @@
-import { AgGridToolbarAction } from './ag-grid-toolbar-action';
-
-export interface AgGridContext {
-  actions?: AgGridToolbarAction[];
-  quickFilterText?: string;
-}
